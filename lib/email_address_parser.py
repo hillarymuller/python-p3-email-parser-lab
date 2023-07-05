43ea972da24f39211b57be9e485ce34418813b69
Hillary Muller <hillaryaward@gmail.com>
2023-07-05 18:38:14 -0500

    split string of email addresses

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,16 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    emails = re.compile(r"[A-z]([A-z0-9][\.]{0,1})+[A-z0-9]+@[A-z0-9]+\.[a-z]{2,}")
+    def parse(self):
+        strings = re.split(r',|\s', self.addresses)
+
+        parsed_emails = set()
+        for string in strings:
+            if self.emails.fullmatch(string):
+                parsed_emails.add(string)
+
+        return sorted(list(parsed_emails))
